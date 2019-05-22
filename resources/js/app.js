4ccf5ae636f44cbc3cb81e4ebf1871c3a75c53d8
liang <1017093063@qq.com>
2019-05-22 17:48:38 +0800

    新增收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -27,6 +29,13 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// 此处需在引入 Vue 之后引入
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
+
 const app = new Vue({
     el: '#app',
 });
+
+
+
