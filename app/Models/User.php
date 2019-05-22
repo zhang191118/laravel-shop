0a53742cb6d75abaa042f52e8f6a6688dd9a9b06
liang <1017093063@qq.com>
2019-05-22 10:15:49 +0800

    邮件验证功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable; 
 
     /**
      * The attributes that are mass assignable.
